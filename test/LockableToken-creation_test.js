f6c5f1b471aee34a13bda03c4e8e3a4f36f322d3
unknown <nitika.goel@somish.com>
2018-06-25 19:03:16 +0530

    Updated test cases

--- test/LockableToken-creation_test.js
+++ test/LockableToken-creation_test.js
@@ -5,7 +5,8 @@ const assertExpectedArguments = require('./utils/assertExpectedArguments')
 
 contract('LockableToken', ([owner]) => {
   const supply = 1000
-  const lockedFor = 'CA'
+  const lockReason = 'CA'
+  const invalidLockReason = 'CD'
   const lockedAmount = 500
   const lockTimestamp = Number(new Date()) / 1000
   const receiver = '0x0758dc7fa551f6E1E5aC48731aD9267fb854CeBB'
@@ -27,21 +28,33 @@ contract('LockableToken', ([owner]) => {
     })
 
     it('has the right balance for the contract owner', async () => {
-      const balance = await token.balances(owner)
+      const balance = await token.balanceOf(owner)
       assert.equal(balance.toNumber(), supply)
     })
 
-    it('has the right transferableBalance for the contract owner', async () => {
-      const balance = await token.transferableBalanceOf(owner)
+    it('has the right total balance for the contract owner', async () => {
+      const balance = await token.totalBalanceOf(owner)
       assert.equal(balance.toNumber(), supply)
     })
 
-    it('transferable balance reduces post locking', async () => {
-      const lock = await token.lock(lockedFor, lockedAmount, lockTimestamp)
-      const balance = await token.balances(owner)
-      const transferableBalance = await token.transferableBalanceOf(owner)
-      assert.equal(balance, supply)
-      assert.equal(transferableBalance, supply - lockedAmount)
+    it('tokens are locked only for a valid reason', async () => {
+      const orig_balance = await token.balanceOf(owner)
+      try {
+        const invalidLock = await token.lock(
+          invalidLockReason,
+          lockedAmount,
+          lockTimestamp
+        )
+        assert()
+      } catch (err) {
+        const validLock = await token.lock(
+          lockReason,
+          lockedAmount,
+          lockTimestamp
+        )
+        const balance = await token.balanceOf(owner)
+        assert.equal(balance, orig_balance - lockedAmount)
+      }
     })
   })
 })
